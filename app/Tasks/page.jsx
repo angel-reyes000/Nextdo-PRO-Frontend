d4c6dd0f0420d68943d24eeea18ac1ee5c5e670f
Angel Reyes <ar731684@gmail.com>
2026-03-26 03:25:31 -0600

    funcionamiento de filtros y busqueda

--- app/Tasks/page.jsx
+++ app/Tasks/page.jsx
@@ -3,6 +3,7 @@
 import styles from '../styles/tasks.module.scss'
 import { FaPlus, FaCalendar, FaSearch } from 'react-icons/fa'
 import { useState, useRef, useEffect } from 'react'
+import Task from './Task.jsx'
 
 let ids = 0;
 let list_tasks = []
@@ -11,11 +12,14 @@ export default function Tasks () {
     const refPriorityHigh = useRef(null)
     const refPriorityMedium = useRef(null)
     const refPriorityLow = useRef(null)
-    const [priorityStateButton, setPriorityStateButton] = useState(null)
+    const [priorityStateButton, setPriorityStateButton] = useState('')
     const [inputCreateTask, setInputCreateTask] = useState('')
     const [inputCreateDescriptionTask, setInputCreateDescriptionTask] = useState('')
     const [inputCreateDeadLineTask, setInputCreateDeadLineTask] = useState('')
     const [tasks, setTasks] = useState(list_tasks)
+    const [inputSearchTask, setInputSearchTask] = useState('')
+    const [selectPriority, setSelectPriority] = useState('all')
+    const [selectDeadLine, setSelectDeadLine] = useState('')
 
     let refHigh = refPriorityHigh.current
     let refMedium = refPriorityMedium.current
@@ -54,6 +58,10 @@ export default function Tasks () {
         console.log("Cambio de prioridad")
     }, [priorityStateButton])
 
+    function Search_engine () {
+        return <Task tasks={tasks} inputSearchTask={inputSearchTask} selectPriority={selectPriority} selectDeadLine={selectDeadLine}/>
+    }
+
     return (
         <>  
             <div className={styles.tasks_window}>
@@ -80,7 +88,7 @@ export default function Tasks () {
                                 <input value={inputCreateDeadLineTask} onChange={(e) => setInputCreateDeadLineTask(e.target.value)} type='date' placeholder='Deadline' id="deadline" name="deadline"></input>
                             </div>
                             <button onClick={() => {
-                                setTasks([...tasks, {id: ids++, title: inputCreateTask, description: inputCreateDescriptionTask, deadline: inputCreateDeadLineTask, priority: priorityStateButton}])
+                                setTasks([...tasks, {id: ids++, title: inputCreateTask, description: inputCreateDescriptionTask, deadline: inputCreateDeadLineTask, priority: priorityStateButton !== '' ? priorityStateButton : 'low'}])
                                 setInputCreateTask('')
                                 setInputCreateDescriptionTask('')
                                 setInputCreateDeadLineTask('')
@@ -94,7 +102,6 @@ export default function Tasks () {
                                 }}>
                                 <FaPlus size={20} style={{display: 'inline-block', alignContent: 'center', marginBottom: '2%'}}/> Add Task
                             </button>
-                            {}
                         </div>
                         {inputCreateTask.length > 0 ? (
                             <div className={styles.description_task}>
@@ -107,20 +114,20 @@ export default function Tasks () {
                     <div className={styles.filters_and_search}>
                         <div className={styles.input_filters_and_search}>
                             <FaSearch size={20}/>
-                            <input placeholder='Search Task...'></input>
+                            <input value={inputSearchTask} onChange={(e) => setInputSearchTask(e.target.value)} placeholder='Search Task...'></input>
                         </div>
                         <div className={styles.select_filters_and_search_filter}>
-                            <select>
+                            <select value={selectPriority} onChange={(e) => setSelectPriority(e.target.value)}>
                                 <option value='all'>Filter by: [All Tasks]</option>
                                 <option value='high'>Filter by: [High Tasks]</option>
-                                <option value='Medium'>Filter by: [Medium Tasks]</option>
-                                <option value='Low'>Filter by: [Low Tasks]</option>
+                                <option value='medium'>Filter by: [Medium Tasks]</option>
+                                <option value='low'>Filter by: [Low Tasks]</option>
                             </select>
                         </div>
                         <div className={styles.select_filters_and_search_sort}>
-                            <select>
-                                <option value='descs'>Sort by: [Deadline Descs]</option>
-                                <option value='ascn'>Sort by: [Deadline Ascn]</option>
+                            <select value={selectDeadLine} onChange={(e) => setSelectDeadLine(e.target.value)}>
+                                <option value='asc'>Sort by: [Deadline Asc]</option>
+                                <option value='desc'>Sort by: [Deadline Descs]</option>                                
                             </select>
                         </div>
                         <div className={styles.priority_filters_and_search}>
@@ -130,30 +137,7 @@ export default function Tasks () {
                         </div>
                     </div>
                     {/* ---------------------------- Tasks -------------------------------*/}
-                    {tasks.map((task) => (
-                        <>
-                            <div key={task.id} className={styles.tasks} style={{borderLeft: `7px solid ${task.priority === 'high' ? 'red' : task.priority === 'medium' ? 'yellow' : 'green'}`}}>
-                                <div className={styles.tasks_checkbox}>
-                                    <input type="checkbox"></input>
-                                </div>
-                                <div className={styles.information_tasks}>
-                                    <div className={styles.header_tasks}>
-                                        <p style={{backgroundColor: `${task.priority === 'high' ? 'red' : task.priority === 'medium' ? 'yellow' : 'green'}`}}>{task.priority ? task.priority : 'Low'}</p>
-                                        <h2>{task.title}</h2>
-                                    </div>
-                                    <div className={styles.description_tasks}>
-                                        <p>{task.description}</p>
-                                    </div>
-                                    <div className={styles.date_tasks}>
-                                        <p><FaCalendar style={{display: 'inline'}}/> {task.deadline}</p>
-                                    </div>
-                                </div>
-                                <div className={styles.three_dots_tasks}>
-                                    <p>...</p>
-                                </div>
-                            </div>
-                        </> 
-                    ))}
+                    <Search_engine />
                 </div>
             </div>
         </>
